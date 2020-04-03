8ff37eeaea129abf7d776710954c6e822253a53a
vikaldevlal <vikaldevlal@gmail.com>
2020-04-03 14:45:20 -0700

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -90,7 +90,7 @@ exports.execute = function (req, res) {
 	
          logData(req);
 	//merge the array of objects.
-	var aArgs = req.body.inArguments;
+	var aArgs = req.inArguments;
 	var inArgs = {};
 	for (var i=0; i<aArgs.length; i++) {  
 		for (var key in aArgs[i]) { 
