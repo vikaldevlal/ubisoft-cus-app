f5a30717727193635e0b0038b516dbfa81b0c7e5
vikaldevlal <vikaldevlal@gmail.com>
2020-04-02 19:25:50 -0700

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -3,76 +3,84 @@ define( function( require ) {
     'use strict';
     
 	var Postmonger = require( 'postmonger' );
-	
 
     var connection = new Postmonger.Session();
-    var toJbPayload = {};
-    var step = 1; 
-	var tokens;
-	var endpoints;
-	
+    var authTokens = {};
+    var payload = {};
     $(window).ready(onRender);
 
-    connection.on('initActivity', function(payload) {
-        var priority;
+    connection.on('initActivity', initialize);
+    connection.on('requestedTokens', onGetTokens);
+    connection.on('requestedEndpoints', onGetEndpoints);
 
-        if (payload) {
-            toJbPayload = payload;
-            console.log('payload : ',payload);
-            
-			//merge the array of objects.
-			var aArgs = toJbPayload['arguments'].execute.inArguments;
-			var oArgs = {};
-			for (var i=0; i<aArgs.length; i++) {  
-				for (var key in aArgs[i]) { 
-					oArgs[key] = aArgs[i][key]; 
-				}
-			}
-			//see createCase for an example on how to use config.json data during activity configuration.            
-        }
-        
-	connection.trigger('updateButton', {
-            button: 'next',
-            text: 'done',
-            visible: true
-        });	
-        
-    });
-
-    connection.on('requestedTokens', function(data) {
-		if( data.error ) {
-			console.error( data.error );
-		} else {
-			tokens = data;
-		}        
-    });
-
-    connection.on('requestedEndpoints', function(data) {
-		if( data.error ) {
-			console.error( data.error );
-		} else {
-			endpoints = data;
-		}        
-    });
-
-    
+    connection.on('clickedNext', save);
+   
     function onRender() {
+        // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
 
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
 
-        //setup UI events here.
-    };
+    }
 
-    
+    function initialize(data) {
+        console.log('Inside initialize : data : '+data);
+        if (data) {
+            payload = data;
+        }
+        
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
+
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+
+       console.log('Inside initialize : inArguments : '+inArguments);
+
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
+                
+              console.log('Inside initialize : each inArguments : '+inArguments);
+            });
+        });
+
+        connection.trigger('updateButton', {
+            button: 'next',
+            text: 'done',
+            visible: true
+        });
+    }
+
+    function onGetTokens(tokens) {
+        console.log('Inside onGetTokens : '+tokens);
+        authTokens = tokens;
+    }
+
+    function onGetEndpoints(endpoints) {
+        
+        console.log('Inside onGetEndpoints : '+endpoints);
+    }
 
     function save() {
 
-		//inArguments key:value pairs can be set here to be used by the first custom activity...see createCase.
-		
-		toJbPayload.metaData.isConfigured = true;  //this is required by JB to set the activity as Configured.
-        connection.trigger('updateActivity', toJbPayload);
-    }; 
-    	 
+        payload['arguments'].execute.inArguments = [{
+            "ContactKey":"{{Contact.Key}}",
+			"FirstName":"{{Contact.Attribute.LatestWebhook.FirstName}}",
+            "emailAddress": "{{Contact.Attribute.LatestWebhook.Email}}",
+			"region": "{{Contact.Attribute.LatestWebhook.Region}}",
+			"JourneyDefinitionId": "{{Context.DefinitionId}}",
+			"JourneyDefinitionInstanceId": "{{Context.Context.DefinitionInstanceId}}"
+        }];
+        
+        payload['metaData'].isConfigured = true;
+
+        console.log('payload : '+payload);
+        connection.trigger('updateActivity', payload);
+    }
+
+
 });
