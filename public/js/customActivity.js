219e5a7dc82ba5fe7c0e72fba876152b3e369882
vikaldevlal <vikaldevlal@gmail.com>
2020-04-02 18:31:55 -0700

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,42 +1,74 @@
-'use strict';
+define( function( require ) {
 
-define(function (require) {
-	var Postmonger = require('postmonger');
-	var connection = new Postmonger.Session();
-	var payload = {};
-
-	$(window).ready(function () {
-		connection.trigger('ready');
-		connection.trigger('requestInteraction');
-	});
-
-	function initialize (data) {
-		if (data) {
-			payload = data;
-		}
-	}
+    'use strict';
+    
+	var Postmonger = require( 'postmonger' );
 	
 
-	function save () {
-		payload['arguments'] = payload['arguments'] || {};
-		payload['arguments'].execute = payload['arguments'].execute || {};
-
-		payload['arguments'].execute.inArguments = [{
-            "ContactKey":"{{Contact.Key}}",
-			"FirstName":"{{Contact.Attribute.LatestWebhook.FirstName}}",
-            "emailAddress": "{{Contact.Attribute.LatestWebhook.Email}}",
-			"region": "{{Contact.Attribute.LatestWebhook.Region}}",
-			"JourneyDefinitionId": "{{Context.DefinitionId}}",
-			"JourneyDefinitionInstanceId": "{{Context.DefinitionInstanceId}}"
-        }];
-		payload['metaData'] = payload['metaData'] || {};
-		payload['metaData'].isConfigured = true;
-
-		console.log(JSON.stringify(payload));
-
-		connection.trigger('updateActivity', payload);
-	}
-
-	connection.on('initActivity', initialize);
+    var connection = new Postmonger.Session();
+    var toJbPayload = {};
+    var step = 1; 
+	var tokens;
+	var endpoints;
 	
+    $(window).ready(onRender);
+
+    connection.on('initActivity', function(payload) {
+        var priority;
+
+        if (payload) {
+            toJbPayload = payload;
+            console.log('payload : ',payload);
+            
+			//merge the array of objects.
+			var aArgs = toJbPayload['arguments'].execute.inArguments;
+			var oArgs = {};
+			for (var i=0; i<aArgs.length; i++) {  
+				for (var key in aArgs[i]) { 
+					oArgs[key] = aArgs[i][key]; 
+				}
+			}
+			//see createCase for an example on how to use config.json data during activity configuration.            
+        }
+        
+		
+        
+    });
+
+    connection.on('requestedTokens', function(data) {
+		if( data.error ) {
+			console.error( data.error );
+		} else {
+			tokens = data;
+		}        
+    });
+
+    connection.on('requestedEndpoints', function(data) {
+		if( data.error ) {
+			console.error( data.error );
+		} else {
+			endpoints = data;
+		}        
+    });
+
+    
+    function onRender() {
+        connection.trigger('ready');
+
+        connection.trigger('requestTokens');
+        connection.trigger('requestEndpoints');
+
+        //setup UI events here.
+    };
+
+    
+
+    function save() {
+
+		//inArguments key:value pairs can be set here to be used by the first custom activity...see createCase.
+		
+		toJbPayload.metaData.isConfigured = true;  //this is required by JB to set the activity as Configured.
+        connection.trigger('updateActivity', toJbPayload);
+    }; 
+    	 
 });
